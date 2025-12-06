6e0831cbcbd09eb280a214e82196e9cbaa028a02
Abithsingh Saravanan <abithsinghs@opentext.com>
2025-12-06 10:42:35 +0530

    monthly calendar initial commit

---
+++ calender/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Calendar from "./Calendar";
+import Verses from "./Verse";
+
+function App() {
+  return (
+    <div className="App">
+      <Calendar />
+      <Verses />
+    </div>
+  );
+}
+
+export default App;
